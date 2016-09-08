36e5cebb935e5de9f7ca00355a264c176c67d277
Drake Ballard <drakeballard@gmail.com>
2016-09-07 20:35:32 -0500

    Adding the questions 1-6 for the game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,19 @@
 alert("Welcome to the NFL Trivia Challenge");
+
+// // Questions
+// Who has the most wins as a head coach in the NFL? (George Halas, Curly Lambeau, Tom Landry, Don Shula)
+//   Don Shula
+//
+// Which NFL team features a helmet decal only on one side of the helmet? (Houston Texans, Jacksonville Jaguars, Pittsburgh Steelers, Tennessee Titans)
+//   Pittsburgh Steelers
+//
+// Who is the last non-quarterback to win NFL MVP? (Shaun Alexander, Ray Lewis, Adrian Peterson, LaDainian Tomlinson)
+//   Adrian Peterson
+//
+// This current NFL quarterback, a 2010 Pro Bowler, never started a game at QB in college? (Matt Schaub, Matt Cassel, Matt Moore, Matt Flynn)
+//   Matt Cassel
+//
+// How many Heisman Trophy winners have gone on to be MVP of the Super Bowl? (2,3,4,5)
+//   4
+// 4 of the first 5 picks in the 1989 draft -- Troy Aikman, Barry Sanders, Derrick Thomas and Deion Sanders -- are in the Hall of Fame. Who was the bust? (Aundray Bruce, Blair Thomas, Keith McCants, Tony Mandarich)
+//   Tony Mandarich
