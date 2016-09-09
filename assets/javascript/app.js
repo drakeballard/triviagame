440d5220987e32feb79c29f2e648f925d8f0d8f2
Drake Ballard <drakeballard@gmail.com>
2016-09-08 23:06:31 -0500

    questions have been marked active

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,36 +10,32 @@ var timer = 20;
 
 
 
-
-
-
-
-
-
 // Questions --> keep in mind that the correct answers have to be reflected with the array (0,1,2, and etc.)
-var questions =
-// Question: "Who has the most wins as a head coach in the NFL?",
-// Answers: (George Halas, Curly Lambeau, Tom Landry, Don Shula)
-// Correct: 3
+var questions = {
+Question: "Who has the most wins as a head coach in the NFL?",
+Answers: (George Halas, Curly Lambeau, Tom Landry, Don Shula)
+Correct: 3
 //
-// Question: "Which NFL team features a helmet decal only on one side of the helmet?"
-// Answers: (Houston Texans, Jacksonville Jaguars, Pittsburgh Steelers, Tennessee Titans)
-// Correct: 2
+Question: "Which NFL team features a helmet decal only on one side of the helmet?"
+Answers: (Houston Texans, Jacksonville Jaguars, Pittsburgh Steelers, Tennessee Titans)
+Correct: 2
 //
-// Question: "Who is the last non-quarterback to win NFL MVP?"
-// Answers: (Shaun Alexander, Ray Lewis, Adrian Peterson, LaDainian Tomlinson)
-// Correct: 2
+Question: "Who is the last non-quarterback to win NFL MVP?"
+Answers: (Shaun Alexander, Ray Lewis, Adrian Peterson, LaDainian Tomlinson)
+Correct: 2
 //
-// Question: "This current NFL quarterback, a 2010 Pro Bowler, never started a game at QB in college?"
-// Answers: (Matt Schaub, Matt Cassel, Matt Moore, Matt Flynn)
-// Correct: 1
+Question: "This current NFL quarterback, a 2010 Pro Bowler, never started a game at QB in college?"
+Answers: (Matt Schaub, Matt Cassel, Matt Moore, Matt Flynn)
+Correct: 1
 //
-// Question: "How many Heisman Trophy winners have gone on to be MVP of the Super Bowl?"
-//Answers: (2,3,4,5)
-//Correct: 2
+Question: "How many Heisman Trophy winners have gone on to be MVP of the Super Bowl?"
+Answers: (2,3,4,5)
+Correct: 2
 //
-// Question: "4 of the first 5 picks in the 1989 draft -- Troy Aikman, Barry Sanders, Derrick Thomas and Deion Sanders -- are in the Hall of Fame. Who was the bust?"
-// Answers: (Aundray Bruce, Blair Thomas, Keith McCants, Tony Mandarich)
-// Correct: 3
+Question: "4 of the first 5 picks in the 1989 draft -- Troy Aikman, Barry Sanders, Derrick Thomas and Deion Sanders -- are in the Hall of Fame. Who was the bust?"
+Answers: (Aundray Bruce, Blair Thomas, Keith McCants, Tony Mandarich)
+Correct: 3
+}
+
 
 //The game needs to start somehow right here.
