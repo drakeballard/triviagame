f6dc1f030f77dd856d812580c6690a8c16fcc788
Drake Ballard <drakeballard@gmail.com>
2016-12-12 23:01:44 -0600

    timer function is now added

--- assets/javascript/app_new.js
+++ assets/javascript/app_new.js
@@ -1,11 +1,5 @@
  $('#start').on('click', function() {
-   $('#start').remove();
-    for(var i=0; i<questions.length;i++){
-      $('#subwrapper').append('<h2>'+questions[i].question+'</h2>');
-      for(var j=0;j<questions[i].answers.length;j++){
-        $("#subwrapper").append("<input type='radio' name='question-"+i+"' value='"+questions[i].answers[j]+"'>"+questions[i].answers[j])
-      }
-    }
+   game.start();
  })
 
  var questions = [{
@@ -33,3 +27,30 @@
    answers: ["Aundray Bruce", "Blair Thomas", "Keith McCants", "Tony Mandarich"],
    correct: "Tony Mandarich"
  }];
+
+
+ var game = {
+   correct: 0,
+   incorrect:0,
+   counter:90,
+   countdown: function() {
+     //descending
+     game.counter--;
+     $('#counter').html(game.counter);
+     if(game.counter<=0) {
+       console.log("Time's up man!");
+       game.done();
+     }
+   },
+   start: function(){
+     timer = setInterval(game.countdown,1000);
+     $('#subwrapper').prepend('<h2> Time Reamining: <span id="counter">90</span> Seconds</h2>');
+     $('#start').remove();
+      for(var i=0; i<questions.length;i++){
+        $('#subwrapper').append('<h2>'+questions[i].question+'</h2>');
+        for(var j=0;j<questions[i].answers.length;j++){
+          $("#subwrapper").append("<input type='radio' name='question-"+i+"' value='"+questions[i].answers[j]+"'>"+questions[i].answers[j])
+        }
+      }
+    }
+  }
